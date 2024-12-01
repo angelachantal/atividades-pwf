bce4fb3fcfbc40f6d4a1d12d2757dc95dcc0f19b
angelachantal <159492508+angelachantal@users.noreply.github.com>
2024-12-01 14:58:45 -0300

    exercicios-js-05-2

--- pwf.exercicios-js-05/script.js
+++ pwf.exercicios-js-05/script.js
@@ -14,3 +14,32 @@ ocultarcaixa.addEventListener('click', () =>{
     resultado.style.visibility = 'hidden';
 });
 
+// exibircaixa.addEventListener('click', () =>{
+//     function visivel (resultado){
+//         return resultado.style.visibility = 'visible';
+//     };
+//     if (visivel){
+//         resultado.style.visibility = 'hidden';
+//     }else{
+//         resultado.style.visibility = 'visible';
+//     }
+// })
+
+let enviartexto = getById('enviartexto');
+enviartexto.addEventListener('click', () =>{
+    let inserirtexto = getById('inserirtexto').value;
+    let texto = getById('texto')
+    texto.innerHTML = inserirtexto
+})
+
+// let fontSizeOriginal;
+
+let aumentartexto = getById('aumentarfonte');
+aumentartexto.addEventListener('click', () =>{
+
+})
+
+let diminuirfonte = getById('diminuirfonte');
+diminuirfonte.addEventListener('click', ()=>{
+
+})
